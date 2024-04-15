3a59398d23674b1724873e62dc2149693b455264
furkantaglik <furkantaglik2334@gmail.com>
2024-04-15 11:35:39 +0300

    client configurations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,3 @@
-function App() {
-  return (
-    <>
-      <div className="flex justify-center items-center h-screen bg-black">
-        <div
-          style={{
-            filter: " drop-shadow(0 0 2rem blue)",
-          }}
-        >
-          <h1 className="md:text-5xl text-2xl font-extrabold italicgit text-indigo-300">
-            Welcome to Think Tech Talk
-          </h1>
-        </div>
-      </div>
-    </>
-  );
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-export default App;
